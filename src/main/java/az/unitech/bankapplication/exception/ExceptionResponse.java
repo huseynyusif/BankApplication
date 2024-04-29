72dcb736065dbfdd330f2b8c9b8f90111c301ea9
kfatullayev <kanan.fatullayev@pashabank.az>
2024-04-29 21:49:20 +0400

    Update package structure and error handler logic

---
+++ src/main/java/az/unitech/bankapplication/exception/ExceptionResponse.java
@@ -0,0 +1,10 @@
+package az.unitech.bankapplication.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ExceptionResponse {
+    private String code;
+}
